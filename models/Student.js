3aaa4e814323137b2dde67b9858662b3ab90f2d0
mike-carbone <mfcbone2@gmail.com>
2018-12-05 01:33:01 -0500

    :fire: Clean student model and fix buddy route

--- models/Student.js
+++ models/Student.js
@@ -26,49 +26,6 @@ const StudentSchema = new Schema({
 	}]
 });
 
-// class User{
-// 	constructor(data){
-// 		this.data = data;
-// 	}
-
-// 	verify(){
-// 		return true;
-// 	}
-
-// 	getUserFirstName(){
-// 		return this.data["student-details"][0]['first-name'];
-// 	}
-
-// 	getUserLastName(){
-// 		return this.data["student-details"][0]['last-name'];
-// 	}
-
-// 	getUserFullName(){
-// 		return this.getUserFirstName() + ' ' + this.getUserLastName();
-// 	}
-
-// 	getParentPhone(){
-// 		return this.data["parent-details"][0].phone;
-// 	}
-
-// 	getParentFirstName(){
-// 		return this.data["parent-details"][0]['first-name'];
-// 	}
-
-// 	getParentLastName(){
-// 		return this.data["parent-details"][0]['last-name'];
-// 	}
-
-// 	getParentFullName(){
-// 		return this.getParentFirstName() + ' ' + this.getParentLastName();
-// 	}
-// }
-
-// module.exports = {
-// 	model: mongoose.model('student', StudentSchema),
-// 	user: User
-// }
-
 module.exports = mongoose.model('Student', StudentSchema);
 
 
